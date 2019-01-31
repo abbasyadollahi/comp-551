80609ad3e3bee0ee962a0c2a47d4479983d000e5
Abbas Yadollahi <abbasyadollahi@outlook.com>
2019-01-30 21:47:43 -0500

    More features for ur headtop

--- src/features.py
+++ src/features.py
@@ -76,37 +76,44 @@ class PreprocessData:
         return most_common
 
 
+    def feature_num_curse_words(self, data):
+        return [self.count_curses(dp['text']) for dp in data]
+
+
     def feature_num_capitals(self, data):
         return [sum(1 for c in dp['text'] if c.isupper()) for dp in data]
-
-
-    def feature_sentiment(self, data):
-        return [self.sentiment_analysis(dp['text'])['compound'] for dp in data]
-
-
-    def feature_curse_words(self, data):
-        return [self.count_curses(dp['text']) for dp in data]
 
 
     def feature_num_words(self, data):
         return [len(dp['split_text']) for dp in data]
 
 
+    def feature_sentiment(self, data):
+        return [self.sentiment_analysis(dp['text'])['compound'] for dp in data]
+
+
+    def feature_links(self, data):
+        url = ['http', 'www', '.com', '.ca']
+        return [1 if any(s in dp['text'] for s in url) else 0 for dp in data]
+
+
     def compute_features(self, data):
         self.initialize(data)
 
         top_words = self.feature_most_common_words(data)
+        num_curse_words = self.feature_num_curse_words(data)
         num_capitals = self.feature_num_capitals(data)
-        sentiment = self.feature_sentiment(data)
-        curse_words = self.feature_curse_words(data)
         num_words = self.feature_num_words(data)
+        sentiment = self.feature_sentiment(data)
+        links = self.feature_links(data)
 
         for i, x in enumerate(self.X):
             x += top_words[i]
+            x.append(num_curse_words[i])
             x.append(num_capitals[i])
-            x.append(sentiment[i])
-            x.append(curse_words[i])
             x.append(num_words[i])
+            x.append(sentiment[i])
+            x.append(links[i])
 
         X = np.array(self.X)
         y = np.array(self.y)
@@ -114,17 +121,17 @@ class PreprocessData:
         return X, y
 
 
+    def count_curses(self, words):
+        return sum(1 for curse in self.curses if curse in words)
+
+
     def sentiment_analysis(self, text):
-        if not os.path.exists(os.path.join(Path.home(), f'nltk_data/sentiment/{self.LEXICON}.zip')):
-            nltk_download('vader_lexicon')
+        if not os.path.exists(f'./sentiment/{self.LEXICON}.zip'):
+            nltk_download('vader_lexicon', download_dir='.')
 
         sia = SentimentIntensityAnalyzer()
         ss = sia.polarity_scores(text)
         return ss
-
-
-    def count_curses(self, words):
-        return sum(1 for curse in self.curses if curse in words)
 
 
 ppd = PreprocessData()
@@ -133,6 +140,7 @@ ppd.compute_most_common_words(train)
 X, y = ppd.compute_features(train)
 # print(X.shape)
 # print(y.shape)
-# print(X[:3])
-# print(y[:3])
-# print(train[:3])
+# for i in range(5):
+#     print(X[i])
+#     print(y[i])
+#     print(train[i])
