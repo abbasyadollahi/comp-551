92faf4d17727ecbe60e0ef004f4ea5f4da2e57ce
Alex Harris <alexander.harris@mail.mcgill.ca>
2019-02-22 11:38:17 -0800

    Changed lstm params

--- project2/src/lstm.py
+++ project2/src/lstm.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Embedding, Dropout, SpatialDropout1D
+from keras.layers import Dense, LSTM, Embedding, Dropout, SpatialDropout1D, GRU
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -13,14 +13,22 @@ from sklearn.model_selection import train_test_split
 from data import load_train
 
 data = load_train()
-reviews, Y = zip(*data)
+reviews, y = zip(*data)
 
-max_features = 2000
+max_features = 5000
 tokenizer = Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(reviews)
 X = tokenizer.texts_to_sequences(reviews)
 X = pad_sequences(X)
 print(len(X))
+
+Y = []
+for v in y:
+	if v == 0:
+		Y.append([1,0])
+	else:
+		Y.append([0,1])
+Y = np.array(Y)
 
 embed_size = 32
 lstm_size = 100
@@ -31,7 +39,8 @@ model = Sequential()
 model.add(Embedding(max_features, embed_size, input_length=X.shape[1]))
 model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=2))
-model.add(LSTM(lstm_size, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(units=lstm_size, dropout=0.2, recurrent_dropout=0.2))
+# model.add(GRU(units=lstm_size, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
