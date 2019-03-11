3a73923051e6066058a4fb25d1d4aba807263a73
Abbas Yadollahi <abbas_y@hotmail.ca>
2019-03-11 19:29:09 -0400

    First shitty model

--- project3/src/data.py
+++ project3/src/data.py
@@ -1,13 +1,22 @@
 import numpy as np
 import pandas as pd
 import os
+import os.path as op
+import matplotlib.pyplot as plt
 
-DATA_DIR = './project3/data'
+DATA_DIR = op.abspath(op.join(__file__, op.pardir, op.pardir, 'data'))
 
-def get_train_data():
-    train_images = pd.read_pickle(os.path.join(DATA_DIR, 'train_images.pkl'))
-    train_labels = pd.read_csv(os.path.join(DATA_DIR, 'train_labels.csv'))
+def load_train():
+    train_images = pd.read_pickle(op.join(DATA_DIR, 'train_images.pkl'))
+    train_labels = pd.read_csv(op.join(DATA_DIR, 'train_labels.csv')).to_numpy()[:, 1]
     return train_images, train_labels
 
-def get_test_data():
-    return pd.read_pickle(os.path.join(DATA_DIR, 'test_images.pkl'))
+def load_test():
+    return pd.read_pickle(op.join(DATA_DIR, 'test_images.pkl'))
+
+# train, csv = load_train()
+# test = load_test()
+# import cv2
+# cv2.imwrite('gang.jpg', train[0])
+# plt.imshow(train[0], cmap='gray')
+# plt.show()
