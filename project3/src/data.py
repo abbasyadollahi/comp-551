053c153e6ba96030b83e01b84dcecc1021998d30
Le Nhat Hung <Le.nhthung@gmail.com>
2019-03-11 17:55:44 -0400

    Create README

--- project3/src/data.py
+++ project3/src/data.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
-import os
+#import os
+from pathlib import Path
 
-DATA_DIR = './project3/data'
+#DATA_DIR = './project3/data'
+
+project_dir = Path(__file__).resolve().parents[1]
+raw_path = project_dir / 'data/raw'
 
 def get_train_data():
-    train_images = pd.read_pickle(os.path.join(DATA_DIR, 'train_images.pkl'))
-    train_labels = pd.read_csv(os.path.join(DATA_DIR, 'train_labels.csv'))
+    train_images = pd.read_pickle(raw_path / 'train_images.pkl')
+    train_labels = pd.read_csv(raw_path / 'train_labels.csv')
     return train_images, train_labels
 
 def get_test_data():
-    return pd.read_pickle(os.path.join(DATA_DIR, 'test_images.pkl'))
+    return pd.read_pickle(raw_path / 'test_images.pkl')
