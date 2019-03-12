37fa33eee8ec2ad92b71380289e8b49c25341a70
Alexander Harris <alexander.harris@mail.mcgill.ca>
2019-03-11 20:22:46 -0400

    CNN

---
+++ project3/src/models.py
@@ -0,0 +1,65 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras.callbacks import ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+
+from data import get_train_data
+
+num_classes = 10
+batch_size = 128
+epochs = 10
+
+img_x, img_y = 64, 64
+
+train_images, train_labels = get_train_data()
+x_train, x_valid, y_train, y_valid = train_test_split(train_images, train_labels, test_size=0.3, random_state=42)
+
+x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 1)
+x_valid = x_valid.reshape(x_valid.shape[0], img_x, img_y, 1)
+x_train = x_train.astype('float32')
+x_valid = x_valid.astype('float32')
+x_train /= 255
+x_valid /= 255
+print(x_train.shape)
+print(y_train.shape)
+
+y_train = to_categorical(y_train, num_classes)
+y_valid = to_categorical(y_valid, num_classes)
+print(y_train.shape)
+print(x_train[0].shape)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_x, img_y, 1)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+annealer = ReduceLROnPlateau(monitor='val_acc', patience=2, verbose=1)
+
+hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_valid, y_valid), callbacks=[annealer])
+score = model.evaluate(x_valid, y_valid, verbose=0)
+
+model.save('./project3/cnn.h5')
+
+print(f'Validation Loss: {score[0]}')
+print(f'Validation Accuracy: {score[1]}')
