50bb816d8293c80d29eb1955c0a5e4475a692ed1
Alexander Harris <alexander.harris@mail.mcgill.ca>
2019-03-11 20:22:46 -0400

    CNN

--- project3/src/data.py
+++ project3/src/data.py
@@ -6,7 +6,7 @@ DATA_DIR = './project3/data'
 
 def get_train_data():
     train_images = pd.read_pickle(os.path.join(DATA_DIR, 'train_images.pkl'))
-    train_labels = pd.read_csv(os.path.join(DATA_DIR, 'train_labels.csv'))
+    train_labels = pd.read_csv(os.path.join(DATA_DIR, 'train_labels.csv')).to_numpy()[:, 1]
     return train_images, train_labels
 
 def get_test_data():
