40cbaf2bbe87a90ac23991b2f3f6702e48d49313
Alex Harris <alexander.harris@mail.mcgill.ca>
2019-03-14 00:05:38 -0400

    Refactored vgg code

--- project3/src/train_model.py
+++ project3/src/train_model.py
@@ -1,6 +1,6 @@
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.models import Sequential, Model, Input
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Average
 from keras.utils import to_categorical
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
@@ -10,6 +10,7 @@ from keras import backend as K
 from matplotlib import pyplot as plt
 
 from data import load_train
+from cnn_vgg10 import VGG10
 
 print(K.tensorflow_backend._get_available_gpus())
 
@@ -35,52 +36,14 @@ print(y_train.shape)
 print(x_train[0].shape)
 
 # Best: Ghetto vgg10 (half layer dimensions), Nadam optimizer
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_x, img_y, 1)))
-model.add(BatchNormalization())
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(2048, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(2048, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-
-# optimizer = Adadelta()
+optimizer = Adadelta()
 # optimizer = Adam()
-optimizer = Nadam()
+# optimizer = Nadam()
 
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer, metrics=['accuracy'])
-print(model.summary())
-
-batch_size = 256  # best 64
-# num_steps = x_train.shape[0] // batch_size
-num_steps = 1000
+batch_size = 64  # best 64
+num_steps = 'auto'
+# num_steps = 1000
 epochs = 200
-
-data_generator = ImageDataGenerator(
-    rotation_range=12,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=1,
-    zoom_range=0.12)
 
 annealer = ReduceLROnPlateau(monitor='val_acc', patience=3, verbose=1, factor=0.5)
 early_stop = EarlyStopping(monitor='val_acc', patience=10, verbose=1)
@@ -90,15 +53,19 @@ checkpoint = ModelCheckpoint(
     monitor='val_acc',
     save_best_only=True)
 
-history = model.fit_generator(
-    data_generator.flow(x_train, y_train, batch_size=batch_size),
-    steps_per_epoch=num_steps,
-    epochs=epochs,
-    verbose=1,
-    validation_data=(x_valid, y_valid),
-    callbacks=[annealer, early_stop, checkpoint])
+model = VGG10(input_shape=(img_x, img_y, 1), num_classes=num_classes, optimizer=optimizer)
 
-score = model.evaluate(x_valid, y_valid, verbose=0)
+history = model.train(
+    x_train, y_train,
+    x_valid, y_valid,
+    batch_size=batch_size,
+    epochs=epochs,
+    datagen=True,
+    num_steps=num_steps,
+    callbacks=[annealer, early_stop, checkpoint]
+    )
+
+score = model.evaluate(x_valid, y_valid)
 
 print(f'Validation Loss: {score[0]}')
 print(f'Validation Accuracy: {score[1]}')
@@ -109,8 +76,8 @@ val_acc = history_dict['val_acc']
 loss = history_dict['loss']
 val_loss = history_dict['val_loss']
 
-if score[1] >= 0.96:
-    model.save(f'./project3/models/cnn_{round(score[1]*100, 2)}%.h5')
+if score[1] >= 0.965:
+    model.save(f'./project3/trained_models/cnn_{round(score[1]*100, 2)}%.h5')
 else:
     print('Model did not exceed baseline validation accuracy, not saving.')
 
