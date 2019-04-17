abd60d25414d3d07eb4c99db196c903f264d719b
Alex Harris <alexander.harris@mail.mcgill.ca>
2019-04-16 19:43:30 -0700

    Train svm

--- project4/src/pipeline.py
+++ project4/src/pipeline.py
@@ -20,26 +20,26 @@ class LemmaTokenizer:
 
     def __call__(self, doc):
         tokens = word_tokenize(doc)
-        # and abs(self.sentiment_strength.get(t, 1)) > 0.4]
-        tokens_no_stop = [t for t in tokens if t not in self.stop_words]
-        return [self.wnl.lemmatize(t) for t in tokens_no_stop]
-        # return [self.wnl.lemmatize(t, pos=self.penn_to_wn(tag)) for t, tag in pos_tag(tokens_no_stop)]
+        # tokens_no_stop = [t for t in tokens if t not in self.stop_words]
+        # return tokens
+        return [self.wnl.lemmatize(t) for t in tokens]
 
-    def penn_to_wn(self, tag):
-        tags = {
-            'JJ': wn.ADJ, 'JJR': wn.ADJ, 'JJS': wn.ADJ,
-            'RB': wn.ADV, 'RBS': wn.ADV, 'RBR': wn.ADV,
-            'NN': wn.NOUN, 'NNS': wn.NOUN, 'NNP': wn.NOUN, 'NNPS': wn.NOUN,
-            'VB': wn.VERB, 'VBD': wn.VERB, 'VBG': wn.VERB, 'VBN': wn.VERB, 'VBP': wn.VERB, 'VBZ': wn.VERB,
-        }
-        return tags.get(tag, wn.NOUN)
-
-def get_vectorizer(max_features, binary=False, bigram=False, tfidf=False):
-    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), max_df=0.5, max_features=max_features)
+def get_vectorizer(max_features, binary=False, bigram=False, trigram=False, tfidf=False):
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), max_features=max_features)
     if tfidf:
-        vectorizer = TfidfVectorizer(tokenizer=LemmaTokenizer(), max_df=0.5, max_features=max_features, norm='l2')
+        vectorizer = TfidfVectorizer(tokenizer=LemmaTokenizer(), max_features=max_features, norm='l2')
     if binary:
         vectorizer.set_params(binary=True)
     if bigram:
         vectorizer.set_params(ngram_range=(1, 2))
+    if trigram:
+        vectorizer.set_params(ngram_range=(1, 3))
     return vectorizer
+
+def linear_svc_pipeline(max_features, bigram=False, trigram=False, tfidf=False):
+    vectorizer = get_vectorizer(max_features, bigram=bigram, tfidf=tfidf)
+    pipeline = Pipeline([
+        ('vect', vectorizer),
+        ('clf', LinearSVC(C=1, tol=1e-4, max_iter=1000))
+    ])
+    return pipeline
---
+++ project4/src/train_svm.py
@@ -0,0 +1,46 @@
+import time
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from data import load_data
+from pipeline import linear_svc_pipeline
+
+def execute_pipeline(title, pipeline, x, y, x_v, y_v):
+    print(title)
+    start = time.time()
+    pipeline.fit(x, y)
+    train_time = time.time()
+    print(f'Training time: {train_time-start}')
+    print(f'Training accuracy: {pipeline.score(x, y)}')
+    print(f'Validation accuracy: {pipeline.score(x_v, y_v)}')
+    print(f'Scoring time: {time.time()-train_time}s')
+    print('')
+
+if __name__ == '__main__':
+    print('Loading data...')
+    start = time.time()
+    mr_data, mr_labels = load_data('MR.pkl')
+    mr_train_data, mr_dev_data, mr_train_labels, mr_dev_labels = train_test_split(mr_data, mr_labels, test_size=0.3, random_state=42)
+    sst_train_data, sst_train_labels, sst_dev_data, sst_dev_labels, sst_test_data, sst_test_labels = load_data('SST2.pkl')
+    print(f'Time to load data: {time.time()-start}s')
+
+    # pipeline = linear_svc_pipeline(max_features=None, bigram=True, tfidf=True)
+    # execute_pipeline('Linear SVC - MR Dataset', pipeline, mr_train_data, mr_train_labels, mr_dev_data, mr_dev_labels)
+    # execute_pipeline('Linear SVC - SST Dataset', pipeline, sst_train_data, sst_train_labels, sst_dev_data, sst_dev_labels)
+
+    param_grid = {
+        'clf__C': [0.01, 0.1, 1, 10],
+        'clf__tol': [1e-4, 1e-5, 1e-6],
+        'clf__max_iter': [500, 1000, 2000]
+    }
+
+    pipeline = linear_svc_pipeline(max_features=None, bigram=True, tfidf=True)
+    cv = GridSearchCV(pipeline, param_grid, iid=False, cv=5, verbose=1, n_jobs=-1)
+    cv.fit(mr_train_data, mr_train_labels)
+    print("Best parameter (CV score=%0.3f):" % cv.best_score_)
+    print(cv.best_params_)
+
+    pipeline = linear_svc_pipeline(max_features=None, bigram=True, tfidf=True)
+    cv = GridSearchCV(pipeline, param_grid, iid=False, cv=5, verbose=1, n_jobs=-1)
+    cv.fit(sst_train_data, sst_train_labels)
+    print("Best parameter (CV score=%0.3f):" % cv.best_score_)
+    print(cv.best_params_)
